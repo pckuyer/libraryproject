c8f51af90d97039efe97430c50387f8a5cf66075
Paul <paul.kuyer@tuta.io>
2022-10-03 14:38:11 +0100

    all core functionality finished

--- library.js
+++ library.js
@@ -1,4 +1,3 @@
-
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -10,32 +9,38 @@ Book.prototype.info = function(){
 	return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
 };
 
-let myLibrary = [];
-
-function addBookToLibrary(book){
-	return mylibrary = myLibrary.push(book);
-}
-
-//manually creating some books
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-const harryPotter = new Book("Harry Potter and the chamber", "J.K. Roling", "412 pages", "read");
-const nijntje = new Book("Nijntje op de schommel", "Dick Bruna", "23 pages", "read");
-
-//adding the books 
-addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(nijntje);
-
 
 function createNewBookCard(book){
 	let emptyBox = document.getElementById('emptyBox');
+	
 	const para = document.createElement('div');
 	para.classList.add('book_card')
-	const text = document.createTextNode(book.info());
+	const text = document.createTextNode(`${book.title}, by ${book.author}. ${book.pages}`);
 	para.appendChild(text);
+	
+	const removeBtnContainer = document.createElement('div');
+	removeBtnContainer.innerHTML = '<i class="fa fa-trash-o removebtn"></i>';
+	removeBtnContainer.addEventListener('click', removeBookCard);
+	para.appendChild(removeBtnContainer);
+	
+	const bookIconContainer = document.createElement('button');
+	if (book.read === "read"){
+		bookIconContainer.innerHTML = 'read';
+		bookIconContainer.classList.add('thisBookHasBeenRead');
+	} else {
+		bookIconContainer.innerHTML = 'not yet read';		
+		bookIconContainer.classList.add('thisBookHasNotYetBeenRead');
+	}
+	bookIconContainer.addEventListener('click', changeBookReadStatus);
+
+	para.appendChild(bookIconContainer);
+	
 	emptyBox.appendChild(para);
 }
 
+function removeBookCard(){
+	event.target.parentElement.parentElement.remove();
+}
 
 
 function addBookFunction() {
@@ -46,27 +51,46 @@ function addBookFunction() {
 	
 	if (title && author && numberOfPages && readStatus) {
 		const book = new Book(title, author, numberOfPages, readStatus);
-		addBookToLibrary(book);
-		// createNewBookCard(book); //this does not work yet due to no storage
+		createNewBookCard(book); 
+		document.querySelector(".createNewBookForm").reset();
+		document.getElementById('formWrapper').style.display = "none";
+	}
+}
+
+function showNewBookForm() {
+	document.getElementById('formWrapper').style.display = "inline";
+}
+
+
+function changeBookReadStatus() {
+	if (event.target.classList.contains("thisBookHasBeenRead")) {
+	event.target.innerHTML = "not yet read";
+	event.target.classList.replace("thisBookHasBeenRead", "thisBookHasNotYetBeenRead");
+	} else {
+	event.target.innerHTML = "read";
+	event.target.classList.replace("thisBookHasNotYetBeenRead", "thisBookHasBeenRead");	
 	}
 }
 
 
-
-
 //waiting for DOM to load
 document.addEventListener('DOMContentLoaded', function() {
-	//placing text on the page. (Should this be placed in a function???)
-	for (let i = 0; i< myLibrary.length; i++){
-		// execute function createNewBookCard
-		createNewBookCard(myLibrary[i]);
-	}
+	
+	// selector for pop up which contains create new book form
+	let newBookButton = document.getElementById("newBookButton");
+	newBookButton.addEventListener('click', showNewBookForm);
 
-
-	//selector for button
+	//selector for submitting form that creates new book button
 	let addBookToLibraryButton = document.getElementById('addBookToLibraryButton');
 	addBookToLibraryButton.addEventListener("click", addBookFunction);
 
 
+
+
+	//selector for changing read status button
+	// let readStatusBtns = document.querySelectorAll('.thisBookHasBeenRead, .thisBookHasNotYetBeenRead');
+	// for (var i = 0; i < readStatusBtns.length; i++) {
+ //    	readStatusBtns[i].addEventListener('click', changeBookReadStatus);
+	// }
 
 });
